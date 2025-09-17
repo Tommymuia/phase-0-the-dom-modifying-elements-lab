8e17e960d0d6e485ac6bbd61ff15a298c66637c3
Tommymuia <muiathomas.mt@gmail.com>
2025-09-17 11:29:49 +0300

    added

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+
+const main = document.querySelector('main#main');
+if (main) {
+  main.remove();
+}
+
+
+const newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = "Thomas Muia is the champion"; 
